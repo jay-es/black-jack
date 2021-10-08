d68b4ccdd548d23da853ff7f4de23e8da64ac6d0
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2021-10-08 22:04:38 +0900

    表示用の関数作成

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,14 +1,21 @@
 use rand::{thread_rng, Rng};
 
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 struct Card {
     mark: char,
     num: u8,
     val: u8,
 }
 
+impl Card {
+    fn disp(self) -> String {
+        format!("{}{}", self.mark, self.num)
+    }
+}
+
 type Deck = Vec<Card>;
 
+/** デッキ作成 */
 fn make_deck() -> Deck {
     let marks = ['C', 'D', 'H', 'S'];
     let mut cards: Deck = Vec::new();
@@ -27,14 +34,45 @@ fn make_deck() -> Deck {
     cards
 }
 
+/** デッキから1枚ひく */
 fn pick_card(deck: &mut Deck) -> Card {
     let index = thread_rng().gen_range(0..deck.len());
     deck.remove(index)
 }
 
+/** カードの合計点数を計算 */
+fn get_point(cards: &[Card]) -> u8 {
+    cards.iter().map(|x| x.val).sum::<u8>()
+}
+
+/** カードを表示用の文字列に */
+fn join_cards(cards: &[Card]) -> String {
+    cards
+        .iter()
+        .map(|x| x.disp())
+        .collect::<Vec<String>>()
+        .join(" ")
+}
+
+/** 場を表示 */
+fn show_table(my_cards: &[Card], his_cards: &[Card], done: bool) {
+    print!("Mine: {} [{}]", get_point(my_cards), join_cards(my_cards));
+
+    print!("  His: ");
+    if done {
+        print!("{} [{}]", get_point(his_cards), join_cards(his_cards));
+    } else {
+        print!("** [{} **]", his_cards[0].disp());
+    }
+
+    println!();
+}
+
 fn main() {
     let mut deck = make_deck();
-    println!("{:?}", pick_card(&mut deck));
-    println!("{:?}", pick_card(&mut deck));
-    println!("input: {}", deck.len());
+    let mut my_cards = vec![pick_card(&mut deck), pick_card(&mut deck)];
+    let mut his_cards = vec![pick_card(&mut deck), pick_card(&mut deck)];
+
+    show_table(&my_cards, &his_cards, false);
+    show_table(&my_cards, &his_cards, true);
 }
