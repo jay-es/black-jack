ba42e6d2566ef71e71c43524ed00a6923e7cccb0
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2021-10-09 23:19:54 +0900

    全体のフロー完成

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,4 +1,5 @@
 use rand::{thread_rng, Rng};
+use std::io;
 
 #[derive(Clone, Copy, Debug)]
 struct Card {
@@ -54,18 +55,33 @@ fn join_cards(cards: &[Card]) -> String {
         .join(" ")
 }
 
+/** 勝敗判定 */
+fn judge<'a>(my_cards: &'a [Card], his_cards: &'a [Card]) -> &'a str {
+    let my_points = get_point(my_cards);
+    let his_points = get_point(his_cards);
+
+    if my_points > 21 {
+        "Lost"
+    } else if his_points > 21 || my_points > his_points {
+        "Won"
+    } else if my_points < his_points {
+        "Lost"
+    } else {
+        "Draw"
+    }
+}
+
 /** 場を表示 */
 fn show_table(my_cards: &[Card], his_cards: &[Card], done: bool) {
     print!("Mine: {} [{}]", get_point(my_cards), join_cards(my_cards));
 
     print!("  His: ");
     if done {
-        print!("{} [{}]", get_point(his_cards), join_cards(his_cards));
+        println!("{} [{}]", get_point(his_cards), join_cards(his_cards));
+        println!("{}", judge(my_cards, his_cards));
     } else {
-        print!("** [{} **]", his_cards[0].disp());
+        println!("** [{} **]", his_cards[0].disp());
     }
-
-    println!();
 }
 
 fn main() {
@@ -73,6 +89,42 @@ fn main() {
     let mut my_cards = vec![pick_card(&mut deck), pick_card(&mut deck)];
     let mut his_cards = vec![pick_card(&mut deck), pick_card(&mut deck)];
 
-    show_table(&my_cards, &his_cards, false);
+    let lost = loop {
+        show_table(&my_cards, &his_cards, false);
+        println!("pick? Y/n");
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).ok();
+        let input = input.trim().to_lowercase();
+
+        // n なら抜ける
+        if input == "n" {
+            break false;
+        }
+        // 空白と y 以外ならもう一度
+        if !input.is_empty() && input != "y" {
+            continue;
+        }
+
+        my_cards.push(pick_card(&mut deck));
+
+        // 21 点超えたらプレイヤーの負け
+        if get_point(&my_cards) > 21 {
+            break true;
+        }
+    };
+
+    // プレイヤーの負けが確定していなければディーラーのターン
+    if !lost {
+        loop {
+            // 17点以上なら抜ける
+            if get_point(&his_cards) >= 17 {
+                break;
+            }
+
+            his_cards.push(pick_card(&mut deck));
+        }
+    }
+
     show_table(&my_cards, &his_cards, true);
 }
