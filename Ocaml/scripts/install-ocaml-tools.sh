7424fd123f32a2de17e27a5fc41613f05223c171
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2021-12-26 22:27:50 +0900

    テスト追加

--- Ocaml/scripts/install-ocaml-tools.sh
+++ Ocaml/scripts/install-ocaml-tools.sh
@@ -8,6 +8,7 @@ sudo apt-get install -y inotify-tools
 opam install dune
 opam install merlin
 opam install ocamlformat
+opam install ppx_inline_test
 
 # opam pin add ocaml-lsp-server https://github.com/ocaml/ocaml-lsp.git
 opam pin add ocaml-lsp-server https://github.com/ocaml/ocaml-lsp/releases/download/1.4.1/jsonrpc-1.4.1.tbz
--- Ocaml/src/lib/deck.ml
+++ Ocaml/src/lib/deck.ml
@@ -4,11 +4,19 @@ type card = { suit : string; number : int; value : int }
 let suit_of_int n =
   match n mod 4 with 0 -> "C" | 1 -> "D" | 2 -> "H" | _ -> "S"
 
+let%test _ = suit_of_int 0 = "C"
+
+let%test _ = suit_of_int 5 = "D"
+
 (* カード作成 *)
 (* int -> card *)
 let make_card n =
   let number = (n mod 13) + 1 in
   { suit = suit_of_int n; number; value = min number 10 }
+
+let%test _ = make_card 0 = { suit = "C"; number = 1; value = 1 }
+
+let%test _ = make_card 11 = { suit = "S"; number = 12; value = 10 }
 
 (* Fisher-Yates shuffle *)
 let shuffle arr =
@@ -30,7 +38,19 @@ let make_deck () = Array.init 52 make_card |> shuffle |> Array.to_list
 let string_of_card card =
   match card with { suit; number; _ } -> suit ^ string_of_int number
 
+let%test _ = string_of_card { suit = "C"; number = 1; value = 1 } = "C1"
+
+let%test _ = string_of_card { suit = "S"; number = 12; value = 10 } = "S12"
+
 (* カード点数合計 *)
 (* card list -> int *)
 let rec sum_cards cards =
   match cards with [] -> 0 | { value; _ } :: rest -> value + sum_cards rest
+
+let%test _ =
+  sum_cards
+    [
+      { suit = "C"; number = 1; value = 1 };
+      { suit = "S"; number = 12; value = 10 };
+    ]
+  = 11
